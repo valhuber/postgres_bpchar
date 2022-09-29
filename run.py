f12377d7c2633c9846888787bfbb88a950b0a5aa
Val Huber <valjhuber@gmail.com>
2022-09-29 12:13:18 -0700

    readme

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ from sqlalchemy.schema import MetaData
 # db: docker run -d --name postgresql-container --net dev-network -p 5432:5432 -e PGDATA=/pgdata -e POSTGRES_PASSWORD=p apilogicserver/postgres:version1.0.2
 
 engine = create_engine("postgresql://postgres:p@localhost/postgres")
+# engine = create_engine("postgres+psycopg2://postgres:p@localhost/postgres")
+# fails with 'no such module'
+
 metadata = MetaData(engine)
 metadata.reflect(engine)
+
 print("open with metadata")
