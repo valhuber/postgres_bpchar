cfc1bd77f6623cb25e07606b9fd3cf1e9a56e88d
Val Huber <valjhuber@gmail.com>
2022-09-29 14:48:41 -0700

    pg_dialect.ischema_names["bpchar"] =  CHAR

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 import sqlalchemy
 from sqlalchemy.engine import create_engine
 from sqlalchemy.schema import MetaData
+import sqlalchemy.dialects.postgresql.base as pg_dialect
+from sqlalchemy import CHAR, Column, DateTime, Float, ForeignKey, Index, Integer, String, TIMESTAMP, Table, Text, UniqueConstraint, text
 
 # db: docker run -d --name postgresql-container --net dev-network -p 5432:5432 -e PGDATA=/pgdata -e POSTGRES_PASSWORD=p apilogicserver/postgres:version1.0.2
 
+substitute_char = True
+if substitute_char:
+    pg_dialect.ischema_names["bpchar"] =  CHAR
+
 engine = create_engine("postgresql://postgres:p@localhost/postgres")
-# engine = create_engine("postgres+psycopg2://postgres:p@localhost/postgres")
-# fails with 'no such module'
+# engine = create_engine("postgres+psycopg2://postgres:p@localhost/postgres")  # fails with 'no such module'
 
 metadata = MetaData(engine)
 metadata.reflect(engine)
